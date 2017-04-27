9e7bc564741e828354dc4329a7b6ac4aafaabf9e
226286 <wojtassobieszek@interia.pl>
2017-04-27 15:52:43 +0200

    lab6

---
+++ Lab6/tabasc.c
@@ -0,0 +1,31 @@
+#include "tabasc.h"
+
+#include <iostream>
+#include <string.h>
+#include <ctype.h>
+
+
+
+using namespace std;
+
+tabasc::tabasc()
+{
+
+}
+
+tabasc::~tabasc()
+{
+
+}
+
+void tabasc::insert (const char *k, int value)
+{
+
+}
+
+node * tabasc::find (const char *k) const
+{
+
+}
+
+
---
+++ Lab6/tabasc.h
@@ -0,0 +1,45 @@
+#ifndef TABASC_H
+#define TABASC_H
+
+#include <string.h>
+
+struct node
+{
+        node *next;
+        char *klucz;
+        int wartosc;
+        node (const char *k):next (NULL)
+        {
+                klucz = new char[strlen (k) + 1];
+                strcpy (klucz, k);
+        };
+
+        node (const node & s):next (NULL)
+        {
+                if (s.klucz == NULL)
+                        klucz = NULL;
+                else
+                {
+                        klucz = new char[strlen (s.klucz) + 1];
+                        strcpy (klucz, s.klucz);
+                }
+
+                wartosc=s.wartosc;
+        }
+         ~node ()
+        {
+                delete[]klucz;
+        }
+};
+
+class tabasc
+{
+    public:
+        tabasc();
+        virtual ~tabasc();
+        node *find (const char *k) const;
+        void insert (const char *k, int value);
+    protected:
+};
+
+#endif // TABASC_H
